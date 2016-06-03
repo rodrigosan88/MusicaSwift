3ba58c8e1a547f4f581f895ff12ec4c85677203f
iOS <iOS@iMac-de-ENG-2.local>
2016-06-03 15:55:37 -0300

    Commit final

--- Musica/ViewController.swift
+++ Musica/ViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var backgroundMusicPlayer: AVAudioPlayer = AVAudioPlayer()
+    
+    @IBOutlet weak var tfMusica: UITextField!
+    
+    
+    @IBAction func tocar(sender: AnyObject) {
+        playBackgroundMusic(tfMusica.text!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func playBackgroundMusic(filename: String) {
+        let url = NSBundle.mainBundle().URLForResource(filename, withExtension: nil)
+        guard let newURL = url else {
+            print("Could not find file: \(filename)")
+            return
+        }
+        do {
+            backgroundMusicPlayer = try AVAudioPlayer(contentsOfURL: newURL)
+            backgroundMusicPlayer.numberOfLoops = -1
+            backgroundMusicPlayer.prepareToPlay()
+            backgroundMusicPlayer.play()
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
 
 }
 
